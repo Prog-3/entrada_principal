107292e0c5db2fc1f65b91c90e50bdb2a628e612
Gabriel Leiva <gabrielleiva009@gmail.com>
2020-09-28 19:53:13 -0300

    funcion guardar persona -agregado boton registro

--- primer_registro_entrada.php
+++ primer_registro_entrada.php
@@ -7,16 +7,22 @@
     $telefono_valor=$_POST['telefono'];
     $temperatura_valor=$_POST['temperatura'];
     $direccion_valor=$_POST['direccion'];
+    
+    $insert_persona ="INSERT INTO persona(dni, nombre, apellido, telefono) VALUES ('$dni_valor','$nombre_valor','$apellido_valor','$telefono_valor')";
 
-    $insert_persona ="INSERT INTO persona(dni, nombre, apellido, telefono) 
-    VALUES ($dni_valor,$nombre_valor,$apellido_valor,$telefono_valor)";
-
-    mysqli_query($conn,$insert_persona);
-
+    $ingreso= mysqli_query($conn,$insert_persona);
+    
+/*
     $consulta_d ="SELECT * FROM direccion WHERE nombre_direccion=$direccion_valor";
 
     $resultado_consulta = mysqli_query($conn,$consulta_d);
+
+    echo $resultado_consulta;
+
     $total=mysqli_fetch_array($resultado_consulta);
+
+    echo $total;
+
     echo count($total);
     if(count($total)==0){
         $insert_direccion ="INSERT INTO direccion('nombre_direccion') VALUES ($direccion_valor)";
@@ -26,10 +32,10 @@
     $insert_registro = "INSERT INTO `registro_entrada`(`fecha_hora`, `temperatura`, `persona_dni`, `direccion_id`) 
     VALUES (NOW(),$temperatura_valor,$dni_valor,[value-4],$total[0])";
     mysqli_query($conn,$insert_registro);
+    
+*/
+    
+    echo 'persona registrada';
     mysqli_close($conn);
-
-    
-    
-
 
 ?>
