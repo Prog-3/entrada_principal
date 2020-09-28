f0f68b150e18197eda9d4f40e9882a537517beb3
alejandro009 <31078875+alejandro009@users.noreply.github.com>
2020-09-28 11:05:37 -0300

    cambio de primer registro entraada

--- consulta.php
+++ consulta.php
@@ -1,20 +1,32 @@
 
 
 <?php
+//abre session para trabajar variables entre paginas
 session_start();
+
+//conexion con la base de dato
     require 'conexion.php';
+
+//obtenemos dni de la pagina index atraves de POST  
     $dni1 = $_POST['dni'];
-    
+
+//select en la base de datos tabla persona
     $consulta="SELECT * FROM persona WHERE dni = $dni1";
-    
+
+//consulta realizada en la base de datos para saber si la persona ya existe
     $resultado = mysqli_query($conn,$consulta);
+
+//convertimos la consulta en array para trabajarlo
     $a=mysqli_fetch_array($resultado);
+
+//mustra la cantidad de elementos del array
     echo count($a);
-    if (count($a)>0) {
-        $_SESSION['resultado']=$a;
-        header("location: registroentrada.php");
+
+    if (count($a)>0) { //preguntamos si la cantidad de elementos del array es mayor a 0
+        $_SESSION['resultado']=$a;//inviamos el array obtenido atravez de session
+        header("location: registroentrada.php");//incluimos al consulta.php el archivo registroentrada.php
     }else{
-        header("location: registropersona.html");
+        header("location: registropersona.html");//incluimos al consulta.php el archivo registropersona.php
     }
     
 ?>
--- primer_registro_entrada.php
+++ primer_registro_entrada.php
@@ -1,7 +1,35 @@
 <?php
     require 'conexion.php';
-    $_POST['dni'];
-    var nombre=
-    var apellido=
-    var dni=
+    
+    $nombre_valor=$_POST['nombre'];
+    $apellido_valor=$_POST['apellido'];
+    $dni_valor=$_POST['dni'];
+    $telefono_valor=$_POST['telefono'];
+    $temperatura_valor=$_POST['temperatura'];
+    $direccion_valor=$_POST['direccion'];
+
+    $insert_persona ="INSERT INTO persona(dni, nombre, apellido, telefono) 
+    VALUES ($dni_valor,$nombre_valor,$apellido_valor,$telefono_valor)";
+
+    mysqli_query($conn,$insert_persona);
+
+    $consulta_d ="SELECT * FROM direccion WHERE nombre_direccion=$direccion_valor";
+
+    $resultado_consulta = mysqli_query($conn,$consulta_d);
+    $total=mysqli_fetch_array($resultado_consulta);
+    echo count($total);
+    if(count($total)==0){
+        $insert_direccion ="INSERT INTO direccion('nombre_direccion') VALUES ($direccion_valor)";
+        mysqli_query($conn,$insert_direccion);
+    }
+
+    $insert_registro = "INSERT INTO `registro_entrada`(`fecha_hora`, `temperatura`, `persona_dni`, `direccion_id`) 
+    VALUES (NOW(),$temperatura_valor,$dni_valor,[value-4],$total[0])";
+    mysqli_query($conn,$insert_registro);
+    mysqli_close($conn);
+
+    
+    
+
+
 ?>
