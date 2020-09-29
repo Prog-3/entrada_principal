b0f1c046671f8e52591f2d563410ea58cb68de73
alejandro009 <31078875+alejandro009@users.noreply.github.com>
2020-09-29 10:27:09 -0300

    nada nuevo

--- primer_registro_entrada.php
+++ primer_registro_entrada.php
@@ -11,29 +11,26 @@
     $insert_persona ="INSERT INTO persona(dni, nombre, apellido, telefono) VALUES ('$dni_valor','$nombre_valor','$apellido_valor','$telefono_valor')";
 
     $ingreso= mysqli_query($conn,$insert_persona);
-    
-/*
-    $consulta_d ="SELECT * FROM direccion WHERE nombre_direccion=$direccion_valor";
+    mysqli_close($conn);
+    require 'conexion.php';
 
-    $resultado_consulta = mysqli_query($conn,$consulta_d);
+    $consulta_d ="SELECT * FROM direccion WHERE nombre_direccion='$direccion_valor'";
 
-    echo $resultado_consulta;
-
-    $total=mysqli_fetch_array($resultado_consulta);
-
-    echo $total;
-
-    echo count($total);
-    if(count($total)==0){
-        $insert_direccion ="INSERT INTO direccion('nombre_direccion') VALUES ($direccion_valor)";
+    $resultado_consulta=mysqli_query($conn,$consulta_d);
+    $a=mysqli_fetch_array($resultado_consulta);
+    echo $a;
+    if($a == null){
+        
+        echo $direccion_valor;
+        $insert_direccion ="INSERT INTO direccion(nombre_direccion) VALUES ('$direccion_valor')";
         mysqli_query($conn,$insert_direccion);
     }
-
-    $insert_registro = "INSERT INTO `registro_entrada`(`fecha_hora`, `temperatura`, `persona_dni`, `direccion_id`) 
-    VALUES (NOW(),$temperatura_valor,$dni_valor,[value-4],$total[0])";
+    
+    $insert_registro = "INSERT INTO registro_entrada(fecha_hora, temperatura, persona_dni, direccion_id) 
+    VALUES ('NOW()','$temperatura_valor','$dni_valor',$a[0])";
     mysqli_query($conn,$insert_registro);
     
-*/
+
     
     echo 'persona registrada';
     mysqli_close($conn);
